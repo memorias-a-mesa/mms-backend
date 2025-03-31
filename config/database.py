f5efc62309032100c29f3612acf36c5a229e1360
iscarso <iscarso@ford.com>
2025-03-31 12:33:49 -0300

    fix(MMS-47): remove database file

--- config/database.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URI = "mongodb+srv://isabellescarso:isabelle@cluster0.xqtwo.mongodb.net/"
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["memorias-a-mesa"]
-recipes_collection = db["receitas"]
