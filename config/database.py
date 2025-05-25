5aa75b1752219c4ea9ce06d17ec71934ea8eb787
Cainã Jose <josecainan21@gmail.com>
2025-05-25 18:23:03 -0300

    Atualização

--- config/database.py
+++ config/database.py
@@ -1,11 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
+import certifi
 
 
 MONGO_URL = os.environ.get("MONGO_URL")
 
 
-client = AsyncIOMotorClient(MONGO_URL, tls=True, tlsAllowInvalidCertificates=True)
+client = AsyncIOMotorClient(MONGO_URL, tls=True, tlsCAFile=certifi.where())
 db = client["memorias-a-mesa"]
 recipes_collection = db["receitas"]
 user_collection = db["usuarios"]
