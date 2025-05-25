b5eec68d368c114580707c1d18386c241a5f46bb
Cainã Jose <josecainan21@gmail.com>
2025-05-25 18:15:22 -0300

    atualização

--- config/database.py
+++ config/database.py
@@ -5,7 +5,7 @@ import os
 MONGO_URL = os.environ.get("MONGO_URL")
 
 
-client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL, tls=True, tlsAllowInvalidCertificates=True)
 db = client["memorias-a-mesa"]
 recipes_collection = db["receitas"]
 user_collection = db["usuarios"]
